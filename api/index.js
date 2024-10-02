0d75011707f97c2023a9e060e15382f4a1129106
Rohitsj627 <158316834+bitwizrsj@users.noreply.github.com>
2024-10-03 04:24:41 +0530

    before deploying changes

--- api/index.js
+++ api/index.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('Error connecting to MongoDB:', err);
   });
 
+
 const app = express();
 
 // Middleware to parse JSON requests
@@ -46,7 +47,7 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
 // Serve static files from the client directory
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 // Catch-all route to handle client-side routing
 app.get('*', (req, res) => {
