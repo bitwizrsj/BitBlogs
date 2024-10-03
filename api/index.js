e8e4b4368b2c4e69dc8a850f72d3b8875645ef88
Rohitsj627 <158316834+bitwizrsj@users.noreply.github.com>
2024-10-03 05:42:40 +0530

    change indes.js

--- api/index.js
+++ api/index.js
@@ -9,59 +9,35 @@ const cookieParser = require('cookie-parser');
 const commentRoutes = require('./routes/comment.route.js') ;
 const path = require('path');
 
-// Load environment variables from .env file
 dotenv.config();
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB is connected.');
+    console.log('MongoDb is connected');
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+    console.log(err);
   });
+
+
 
 const app = express();
 
-// Middleware to parse JSON requests
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(`${req.method} request made to: ${req.url}`);
-  console.log('Request Body:', req.body);
-  next();
-});
-
 app.use(cookieParser());
 
-// Enable CORS for all origins (you can restrict it to specific origins if needed)
-app.use(cors());
 
-// You can also specify a more restrictive CORS policy
-// app.use(cors({ origin: 'http://localhost:5175', credentials: true }));
 
-// Routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-// Serve static files from the client directory
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// Catch-all route to handle client-side routing
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
 });
 
 // Server Start
