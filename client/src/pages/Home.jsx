855a9c35c51b44f69f4464cd2be0cc5cbecc5359
bitwizrsj <158316834+bitwizrsj@users.noreply.github.com>
2024-10-10 11:52:29 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
         >
           View all posts
         </Link>
-
+        </div>
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
